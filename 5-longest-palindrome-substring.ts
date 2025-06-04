2f3a0d737911927cceed12f0665940525cb57f01
mbardakov <mbardakov@gmail.com>
2025-06-04 15:16:31 -0400

    solution 5 marginally more efficient

--- 5-longest-palindrome-substring.ts
+++ 5-longest-palindrome-substring.ts
@@ -1,7 +1,7 @@
 function longestPalindrome(s: string): string {
-	let longestSoFar: string = '';
+	let longestSoFar: string = `${s[0]}`;
 	for (let i = 0; i < s.length; i++) {
-		for (let j = i; j < s.length; j++) {
+		for (let j = i + 1; j < s.length; j++) {
 			let substring = s.substring(i,j+1)
 			if (isPalindrome(substring) && substring.length > longestSoFar.length) {
 				longestSoFar = substring;
@@ -12,7 +12,7 @@ function longestPalindrome(s: string): string {
 };
 
 function isPalindrome(s: string): boolean {
-	for (let i = 0; i < s.length; i++) {
+	for (let i = 0; i < s.length/2; i++) {
 		if (s[i] !== s[s.length - 1 - i]) {
 			return false;
 		}
