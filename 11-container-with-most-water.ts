8a6a74ff00a6a3765f3288da7075e700003142ae
mbardakov <mbardakov@gmail.com>
2025-06-08 11:44:05 -0400

    solution 11

---
+++ 11-container-with-most-water.ts
@@ -0,0 +1,18 @@
+function maxArea(height: number[]): number {
+	let maxArea = 0;
+	let left = 0;
+	let right = height.length - 1;
+	while (left < right) {
+		let area = (right - left) * Math.min(height[left], height[right]);
+		maxArea = Math.max(area, maxArea);
+		if (height[left] < height[right]) {
+			left++;
+		} else {
+			right--;
+		}
+	}
+	
+	return maxArea;
+};
+
+console.log(maxArea([1,8,6,2,5,4,8,3,7]));
